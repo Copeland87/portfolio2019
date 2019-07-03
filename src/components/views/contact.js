62ae3c006d474488f862ae595e3ccb1d30659979
Zac Copeland <zac@builderdesigns.com>
2019-07-02 21:44:06 -0500

    update because i stupid

--- src/components/views/contact.js
+++ src/components/views/contact.js
@@ -1,7 +1,14 @@
 import React from "react";
 class Contact extends React.Component {
   render() {
-    return <h1>Contact</h1>;
+    return (
+      <div>
+        <form>
+          <input type="text" name="name" value="" />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
   }
 }
 export default Contact;
