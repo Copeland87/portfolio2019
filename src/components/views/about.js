46148b86b5c4e74bb2da2c1920ccc38c82a92403
Zac Copeland <zac@builderdesigns.com>
2019-07-07 04:51:36 -0500

    update to content and styling about

--- src/components/views/about.js
+++ src/components/views/about.js
@@ -6,38 +6,21 @@ class About extends React.Component {
     return (
       <div className="about-container">
         <div className="about-inner-container">
-          <h1>All About Me</h1>
+          <h1>Me</h1>
           <div className="about-content">
             <div className="about-image" />
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and
-              typesetting industry. Lorem Ipsum has been the industry's standard
-              dummy text ever since the 1500s, when an unknown printer took a
-              galley of type and scrambled it to make a type specimen book. It
-              has survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and
-              typesetting industry. Lorem Ipsum has been the industry's standard
-              dummy text ever since the 1500s, when an unknown printer took a
-              galley of type and scrambled it to make a type specimen book. It
-              has survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+            <p className="about-text">
+              My name is Zac Copeland, I'm a programmer who recently moved to
+              California after marrying the love of my life. This is my first
+              time living anywhere further west than Kansas and so far, it's
+              pretty awesome. I have an endless number of hobbies, unfortunately
+              for me anything to do with building or creating something new is
+              interesting to me. Lets see my life: my wife, my doge, video
+              games, 80's music, and almost every movie ever made. On this site
+              you'll find (eventually) links to projects and information about
+              me. If you have any questions feel free to use the form on the
+              contact page to ask questions, say what's up, ... or send random
+              message that make no sense (... you know who you are).
             </p>
           </div>
 
@@ -53,13 +36,12 @@ class About extends React.Component {
                 <div className="hobby-description">
                   <img className="hobby-image hi1" />
                   <p className="hobby-text">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting, remaining
-                    essentially unchanged. It was popularised in the 1960s
+                    Computer code has to be at this point a life pursuit. I
+                    started with basic HTML layouts in yahoo pages and I'm still
+                    trying to learn something new with every day. Different
+                    languages and frameworks all offer their own solutions to
+                    building something new which is why I always have 30 tabs
+                    open.
                   </p>
                 </div>
               </div>
@@ -70,13 +52,11 @@ class About extends React.Component {
                 <div className="hobby-description">
                   <img className="hobby-image hi2" />
                   <p className="hobby-text">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting, remaining
-                    essentially unchanged. It was popularised in the 1960s
+                    Getting my first bike was a challenge since everyone in my
+                    family had a no mentality but at the same time owned or had
+                    owned a bike. I started with a 93' Yamaha and luckily I only
+                    have so much garage space or I'd try to buy and repair every
+                    junked bike I come across. I still have 2 on my wishlist.
                   </p>
                 </div>
               </div>
@@ -87,13 +67,11 @@ class About extends React.Component {
                 <div className="hobby-description">
                   <img className="hobby-image hi3" />
                   <p className="hobby-text">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting, remaining
-                    essentially unchanged. It was popularised in the 1960s
+                    Wood working for me came about as a need in my life for
+                    something that functioned well and endured the time and
+                    projects that are done at my desk. It turns out that fiber
+                    board furniture wasn't well suited for soldering or other
+                    electronic projects.
                   </p>
                 </div>
               </div>
@@ -104,13 +82,10 @@ class About extends React.Component {
                 <div className="hobby-description">
                   <img className="hobby-image hi4" />
                   <p className="hobby-text">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting, remaining
-                    essentially unchanged. It was popularised in the 1960s
+                    I got in to photography as a secondary hobby to building
+                    websites and applications. It's pretty cheese, but I thought
+                    that if I built something from the ground up I wanted the
+                    images to be something that I'd produced myself.
                   </p>
                 </div>
               </div>
