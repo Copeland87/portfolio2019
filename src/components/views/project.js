741496f1c0a8cd5472f61c03a520db8733aa44d2
Zac Copeland <zac@builderdesigns.com>
2019-08-11 05:14:34 -0500

    update to projects page

--- src/components/views/project.js
+++ src/components/views/project.js
@@ -1,7 +1,28 @@
 import React from "react";
 class Project extends React.Component {
   render() {
-    return <h1>Project</h1>;
+    return (
+      <div>
+        <h1>Projects</h1>
+        <div className="hero-text">
+          <p>
+            Welcome to the projects page. At this point in time this section is
+            still under construction as it is a project itself. As I am
+            uploading and incorporating all of my projects on to this webserver,
+            while at the same time still building out new React components and
+            modules for another project for{" "}
+            <a
+              href="https://www.californialivefloors.com"
+              style={{ textDecoration: "underline" }}
+            >
+              California Live Floors.
+            </a>
+            <br />
+            Looking forward to having this built out soon!
+          </p>
+        </div>
+      </div>
+    );
   }
 }
 export default Project;
